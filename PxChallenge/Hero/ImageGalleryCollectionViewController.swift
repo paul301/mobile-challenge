f0a3129f9bf33004299b64ec2558842b4d7d496e
paul301 <paul.floussov@gmail.com>
2017-04-17 21:22:48 -0400

    dynamicly sized images based on aspect ratio

--- PxChallenge/Hero/ImageGalleryCollectionViewController.swift
+++ PxChallenge/Hero/ImageGalleryCollectionViewController.swift
@@ -28,112 +28,121 @@ import SDWebImage
 
 class ImageGalleryViewController: UIViewController {
     @IBOutlet weak var collectionView: UICollectionView!
-    var columns = 3
     var photos : [Photo]?
     var disposeBag = DisposeBag()
-    lazy var cellSize: CGSize = CGSize(width: self.view.bounds.width/CGFloat(self.columns),
-                                    height: self.view.bounds.width/CGFloat(self.columns))
-    
-    
+    var photoSizes = [(photoSize:CGSize, photo:Photo)]()
 
+    override func viewDidLoad() {
+        super.viewDidLoad()
 
-  override func viewDidLoad() {
-    super.viewDidLoad()
-    
-    
-    pxProvider.request(.popularPhotos())
-        .mapObject(PopularPage.self)
-        .subscribe { [weak self] event in
-            switch event {
-            case .next(let page):
-                self?.photos = page.photos
-                self?.collectionView.reloadData()
-            case .error(let error):
-                print(error)
-            default:
-                break
-            }
-        }.disposed(by: disposeBag)
-    
-    
-    
-    collectionView.reloadData()
-    collectionView.indicatorStyle = .white
-  }
+        pxProvider.request(.popularPhotos())
+            .mapObject(PopularPage.self)
+            .subscribe { [weak self] event in
+                switch event {
+                case .next(let page):
+                    self?.photos = page.photos
+                    self?.photoSizes = (ImageHelper.photoSizes(photos: page.photos, viewBounds: (self?.view.bounds)!))
+                    self?.collectionView.reloadData()
+                case .error(let error):
+                    print(error)
+                default:
+                    break
+                }
+            }.disposed(by: disposeBag)
+        
+        collectionView.reloadData()
+        collectionView.indicatorStyle = .white
+    }
 
-  @IBAction func switchLayout(_ sender: Any) {
-    // just replace the root view controller with the same view controller
-    // animation is automatic! Holy
-    let next = (UIStoryboard(name: "ImageGallery", bundle: nil).instantiateViewController(withIdentifier: "imageGallery") as? ImageGalleryViewController)!
-    next.columns = columns == 3 ? 5 : 3
-    hero_replaceViewController(with: next)
-  }
+    @IBAction func switchLayout(_ sender: Any) {
+        // just replace the root view controller with the same view controller
+        // animation is automatic! Holy
+        let next = (UIStoryboard(name: "ImageGallery", bundle: nil).instantiateViewController(withIdentifier: "imageGallery") as? ImageGalleryViewController)!
+        hero_replaceViewController(with: next)
+    }
+    
+    override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
+        let newBounds = CGRect(x: 0, y: 0, width: size.width, height: size.height)
+        if UIDevice.current.orientation.isLandscape {
+            //Landscape
+            photoSizes = ImageHelper.photoSizes(photos: photos!, viewBounds: newBounds)
+            collectionView.reloadData()
+            
+        }
+        else {
+            //Portrait
+            photoSizes = ImageHelper.photoSizes(photos: photos!, viewBounds: newBounds)
+            collectionView.reloadData()
+        }
+    }
 }
 
 extension ImageGalleryViewController: UICollectionViewDataSource {
-  func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-    let vc = (viewController(forStoryboardName: "ImageViewer") as? ImageViewController)!
-    vc.selectedIndex = indexPath
-    vc.photos = photos
-    if let navigationController = navigationController {
-      navigationController.pushViewController(vc, animated: true)
-    } else {
-      present(vc, animated: true, completion: nil)
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        let vc = (viewController(forStoryboardName: "ImageViewer") as? ImageViewController)!
+        vc.selectedIndex = indexPath
+        vc.photos = photos
+        if let navigationController = navigationController {
+            navigationController.pushViewController(vc, animated: true)
+        } else {
+            present(vc, animated: true, completion: nil)
+        }
     }
-  }
-
-  func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-    guard let photos = photos else { return 0 }
-    return photos.count
-  }
-
-  func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-    guard let photos = photos else { return (collectionView.dequeueReusableCell(withReuseIdentifier: "item", for: indexPath) as? ImageCell)! }
     
-    let imageCell = (collectionView.dequeueReusableCell(withReuseIdentifier: "item", for: indexPath) as? ImageCell)!
-    imageCell.imageView.sd_setImage(with: URL(string: photos[indexPath.row].images.first(where: {$0.size == 21})!.url))
-    imageCell.imageView.heroID = "image_\(indexPath.item)"
-    imageCell.imageView.heroModifiers = [.fade, .scale(0.8)]
-    imageCell.imageView.isOpaque = true
-    return imageCell
-  }
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        guard let photos = photos else { return 0 }
+        return photos.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let photos = photos else { return (collectionView.dequeueReusableCell(withReuseIdentifier: "item", for: indexPath) as? ImageCell)! }
+        
+        let imageCell = (collectionView.dequeueReusableCell(withReuseIdentifier: "item", for: indexPath) as? ImageCell)!
+        imageCell.imageView.sd_setImage(with: URL(string: photos[indexPath.row].images.first(where: {$0.size == 21})!.url))
+        imageCell.imageView.heroID = "image_\(indexPath.item)"
+        imageCell.imageView.heroModifiers = [.fade, .scale(0.8)]
+        imageCell.imageView.isOpaque = true
+        return imageCell
+    }
 }
 
 extension ImageGalleryViewController: UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
-  func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-    return cellSize
-  }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return photoSizes[indexPath.row].photoSize
+    }
+    
 }
 
 extension ImageGalleryViewController: HeroViewControllerDelegate {
-  func heroWillStartAnimatingTo(viewController: UIViewController) {
-    if (viewController as? ImageGalleryViewController) != nil {
-      collectionView.heroModifiers = [.cascade(delta:0.015, direction:.bottomToTop, delayMatchedViews:true)]
-    } else if (viewController as? ImageViewController) != nil {
-      let cell = collectionView.cellForItem(at: collectionView.indexPathsForSelectedItems!.first!)!
-      collectionView.heroModifiers = [.cascade(delta: 0.015, direction: .radial(center: cell.center), delayMatchedViews: true)]
-    } else {
-      collectionView.heroModifiers = [.cascade(delta:0.015)]
+    func heroWillStartAnimatingTo(viewController: UIViewController) {
+        if (viewController as? ImageGalleryViewController) != nil {
+            collectionView.heroModifiers = [.cascade(delta:0.015, direction:.bottomToTop, delayMatchedViews:true)]
+        } else if (viewController as? ImageViewController) != nil {
+            let cell = collectionView.cellForItem(at: collectionView.indexPathsForSelectedItems!.first!)!
+            collectionView.heroModifiers = [.cascade(delta: 0.015, direction: .radial(center: cell.center), delayMatchedViews: true)]
+        } else {
+            collectionView.heroModifiers = [.cascade(delta:0.015)]
+        }
     }
-  }
-  func heroWillStartAnimatingFrom(viewController: UIViewController) {
-    view.heroModifiers = nil
-    if (viewController as? ImageGalleryViewController) != nil {
-      collectionView.heroModifiers = [.cascade(delta:0.015), .delay(0.25)]
-    } else {
-      collectionView.heroModifiers = [.cascade(delta:0.015)]
+    func heroWillStartAnimatingFrom(viewController: UIViewController) {
+        view.heroModifiers = nil
+        if (viewController as? ImageGalleryViewController) != nil {
+            collectionView.heroModifiers = [.cascade(delta:0.015), .delay(0.25)]
+        } else {
+            collectionView.heroModifiers = [.cascade(delta:0.015)]
+        }
+        if let vc = viewController as? ImageViewController,
+            let originalCellIndex = vc.selectedIndex,
+            let currentCellIndex = vc.collectionView?.indexPathsForVisibleItems[0],
+            let targetAttribute = collectionView.layoutAttributesForItem(at: currentCellIndex) {
+            collectionView.heroModifiers = [.cascade(delta:0.015, direction:.inverseRadial(center:targetAttribute.center))]
+            if !collectionView.indexPathsForVisibleItems.contains(currentCellIndex) {
+                // make the cell visible
+                collectionView.scrollToItem(at: currentCellIndex,
+                                            at: originalCellIndex < currentCellIndex ? .bottom : .top,
+                                            animated: false)
+            }
+        }
     }
-    if let vc = viewController as? ImageViewController,
-      let originalCellIndex = vc.selectedIndex,
-      let currentCellIndex = vc.collectionView?.indexPathsForVisibleItems[0],
-      let targetAttribute = collectionView.layoutAttributesForItem(at: currentCellIndex) {
-      collectionView.heroModifiers = [.cascade(delta:0.015, direction:.inverseRadial(center:targetAttribute.center))]
-      if !collectionView.indexPathsForVisibleItems.contains(currentCellIndex) {
-        // make the cell visible
-        collectionView.scrollToItem(at: currentCellIndex,
-                                    at: originalCellIndex < currentCellIndex ? .bottom : .top,
-                                    animated: false)
-      }
-    }
-  }
 }
---
+++ PxChallenge/Utils/ImageHelper.swift
@@ -0,0 +1,61 @@
+//
+//  ImageHelper.swift
+//  PxChallenge
+//
+//  Created by Paul Floussov on 2017-04-18.
+//  Copyright © 2017 Paul Floussov. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+public class ImageHelper {
+    
+    static func photoSizes(photos:[Photo], viewBounds: CGRect) -> [(photoSize:CGSize, photo:Photo)] {
+        var output = [(photoSize:CGSize, photo:Photo)]()
+        
+        var index = 0
+        var currentPhotos = [Photo]()
+        while index < photos.count {
+            currentPhotos.append(photos[index])
+            let bestHeight = bestHightForPhotos(photos: currentPhotos, screenWidth: viewBounds.width)
+            
+            if (bestHeight <= Double(viewBounds.height / 4)) {
+                for photo in currentPhotos {
+                    output.append((CGSize(width:aspectRatio(photo: photo) * bestHeight ,height:bestHeight), photo))
+                }
+                currentPhotos = [Photo]()
+            }
+            
+            index += 1
+        }
+        
+        for photo in currentPhotos {
+            let generalHeight = Double(viewBounds.height / 5)
+            
+            output.append((CGSize(width:aspectRatio(photo: photo) * generalHeight, height:generalHeight), photo))
+        }
+        
+        return output
+    }
+    
+    private static func aspectRatio(photo: Photo) -> Double {
+        return Double(photo.width) / Double(photo.height)
+    }
+    
+    private static func photoWidthAtHeight(photo: Photo, height:Int) -> Double {
+        return aspectRatio(photo: photo) * Double(height)
+    }
+    
+    private static func bestHightForPhotos(photos: [Photo], screenWidth: CGFloat) -> Double {
+        let tempHeight = 500
+        var totalWidth: Double = 0
+        for photo in photos {
+            totalWidth += photoWidthAtHeight(photo: photo, height: tempHeight)
+        }
+        
+        let aspectRatio =  Double(tempHeight) / totalWidth
+        
+        return aspectRatio * Double(screenWidth - 0.1)
+    }
+}
